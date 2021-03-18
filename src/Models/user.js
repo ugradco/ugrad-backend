a785459d1d77f14983a5f780c33dfa08fe4bca26
Merve Karakas <mkarakas16@ku.edu.tr>
2021-03-18 12:05:01 +0300

    Initialized backend envirenment with fb, post, user

--- src/Models/user.js
+++ src/Models/user.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    id: Number,
     name: String,
     email: String,
+    alias: String,
+    followed_topics: Array,
+    banned_until: Date,
+    restricted_until: Date,
+    profile_image: String,
+    engagement_score: Number, 
 });
 
 module.exports = mongoose.model('User', userSchema);
